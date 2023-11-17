96464eb40ecfb4b9b13d2a70a280a226264858c3
k4oong <122785908+k4oong@users.noreply.github.com>
2023-11-17 11:45:03 +0800

    bless me

---
+++ anotherCAM3.0/anotherCAM/src/main/java/com/mycompany/anothercam/ReportGenerator/ReportCSVDAO.java
@@ -0,0 +1,198 @@
+/**
+Represents the ReportCSVDAO Object and the following assistance methods.
+A ReportCSVDAO object can be called multiple times
+The class here creates the report format for CSV based on the information given from the camp Array. 
+@author Russel Tan Jun Hong
+@version 1.0
+@since 2023-11-2
+*/
+
+package com.mycompany.anothercam.ReportGenerator;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import com.mycompany.anothercam.Camp;
+import com.opencsv.CSVWriter;
+//import org.apache.poi.xssf.usermodel.XSSFRow;
+//import org.apache.poi.xssf.usermodel.XSSFSheet;
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//import java.io.FileOutputStream;
+//import java.util.Map;
+//import java.util.Set;
+//import java.util.TreeMap;
+
+public class ReportCSVDAO {
+    //private XSSFWorkbook workbook;
+    //private XSSFSheet spreadsheet;
+    private String filename;
+    private String sheetName;
+    private CSVWriter csvFile;
+    private ArrayList<Camp> tempCampArray;
+    private boolean activateAlphabetical;
+    //private FileOutputStream streamOut;
+    /*
+    public ReportCSVDAO(FileOutputStream streamOut,String filename, String sheetName){
+        this.filename = filename;
+        this.sheetName = sheetName;
+        this.streamOut = streamOut;
+        
+    }
+    */
+    
+    /**
+    * The ReportCSVDAO Constructor.
+    * @param tempCampArray The ArrayList containing the camp Array to be printed out
+    * @param filename The filename to specify the creation of the report.
+    */
+    public ReportCSVDAO(ArrayList<Camp> tempCampArray, String filename, boolean activateAlphabetical){
+        this.filename = filename;
+        this.tempCampArray = tempCampArray;
+        this.activateAlphabetical = activateAlphabetical;
+    }
+    
+    /**
+    * Writes the CSV information into the Staff Report.
+    * @return boolean value if the file writing is successful. True for success. False for unsuccessful.
+    */
+    public boolean writeStaffReportInfo(){
+        try{
+            this.csvFile = new CSVWriter(new FileWriter(new File(this.filename)));
+            String[] columnName = {"CAMP Number", "Name", "Date", "Registration Closing Date", "Total slots", "Committee Member Slots", "Staff-in-charge", "Committee Members", "Attendees"};
+            this.csvFile.writeNext(columnName);
+            for(int i=1; i< tempCampArray.size()+1; i++){
+                String slotString = String.valueOf(tempCampArray.get(i-1).getTotalSlot());
+                String committeeString = String.valueOf(tempCampArray.get(i-1).getCampCommitteeSlots());
+                String firstCommittee;
+                // Print the first row of new Camp
+                if(tempCampArray.get(i-1).getCampCommitteeList().size() <=0){
+                    firstCommittee = "NIL";
+                }else{
+                    firstCommittee = tempCampArray.get(i-1).getCampCommitteeList().get(0).getUserID();
+                }
+                String[] firstCol = {String.valueOf(i) ,tempCampArray.get(i-1).getCampName(), tempCampArray.get(i-1).getDateString(),tempCampArray.get(i-1).getRegClosingDateString(),slotString ,committeeString,tempCampArray.get(i-1).getStaffInCharge(), firstCommittee, ""};
+                this.csvFile.writeNext(firstCol);
+                // Print other camp committee members
+                for(int j=1; j< tempCampArray.get(i-1).getCampCommitteeList().size(); j++){
+                    String[] secondData = {"", "", "", "", "", "", "", tempCampArray.get(i-1).getCampCommitteeList().get(j).getUserID(), ""};
+                    this.csvFile.writeNext(secondData);
+                }
+                for(int k=0; k< tempCampArray.get(i-1).getCampStudentList().size(); k++){
+                    String[] teriaryData = {"", "", "", "", "", "", "", "", tempCampArray.get(i-1).getCampStudentList().get(k).getUserID()};
+                    this.csvFile.writeNext(teriaryData);
+                }
+            String[] blankCol = {"", "", "", "", "", "", "", "",""};
+            this.csvFile.writeNext(blankCol);
+            }
+            this.csvFile.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return true;
+    }
+    
+    /**
+    * Writes the CSV information into the Staff Performance Report.
+    * @return boolean value if the file writing is successful. True for success. False for unsuccessful.
+    */
+    public boolean writeStaffPerformanceInfo(){
+        try{
+            this.csvFile = new CSVWriter(new FileWriter(new File(this.filename)));
+            String[] columnName = {"CAMP Number", "Name", "Date", "Committee Members", "Points"};
+            this.csvFile.writeNext(columnName);
+            for(int i=1; i< tempCampArray.size()+1; i++){
+                String slotString = String.valueOf(tempCampArray.get(i-1).getTotalSlot());
+                String committeeString = String.valueOf(tempCampArray.get(i-1).getCampCommitteeSlots());
+                String firstCommittee;
+                String firstCommitteePoints;
+                // Print the first row of new Camp
+                if(tempCampArray.get(i-1).getCampCommitteeList().size() <=0){
+                    firstCommittee = "NIL";
+                    firstCommitteePoints = "NIL";
+                }else{
+                    firstCommittee = tempCampArray.get(i-1).getCampCommitteeList().get(0).getUserID();
+                    firstCommitteePoints = Integer.toString(tempCampArray.get(i-1).getCampCommitteeList().get(0).getPoints());
+                }
+                String[] firstCol = {String.valueOf(i) ,tempCampArray.get(i-1).getCampName(), tempCampArray.get(i-1).getDateString(),firstCommittee, firstCommitteePoints};
+                this.csvFile.writeNext(firstCol);
+                // Print other camp committee members
+                for(int j=1; j< tempCampArray.get(i-1).getCampCommitteeList().size(); j++){
+                    String[] secondData = {"", "", "", tempCampArray.get(i-1).getCampCommitteeList().get(j).getUserID(), Integer.toString(tempCampArray.get(i-1).getCampCommitteeList().get(j).getPoints())};
+                    this.csvFile.writeNext(secondData);
+                }
+            String[] blankCol = {"", "", "", "", ""};
+            this.csvFile.writeNext(blankCol);
+            }
+            this.csvFile.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return true;
+    }
+    
+    /**
+    * Writes the CSV information into the Camp Committee Report.
+    * @return boolean value if the file writing is successful. True for success. False for unsuccessful.
+    */
+    public boolean writeCampCommitteeInfo(){
+        try{
+            this.csvFile = new CSVWriter(new FileWriter(new File(this.filename)));
+            String[] columnName = {"CAMP Number", "Name", "Date", "Registration Closing Date", "Total slots", "Committee Member Slots", "Staff-in-charge", "Committee Members", "Attendees"};
+            this.csvFile.writeNext(columnName);
+            for(int i=1; i< tempCampArray.size()+1; i++){
+                String slotString = String.valueOf(tempCampArray.get(i-1).getTotalSlot());
+                String committeeString = String.valueOf(tempCampArray.get(i-1).getCampCommitteeSlots());
+                String firstCommittee;
+                // Print the first row of new Camp
+                if(tempCampArray.get(i-1).getCampCommitteeList().size() <=0){
+                    firstCommittee = "NIL";
+                }else{
+                    firstCommittee = tempCampArray.get(i-1).getCampCommitteeList().get(0).getUserID();
+                }
+                String[] firstCol = {String.valueOf(i) ,tempCampArray.get(i-1).getCampName(), tempCampArray.get(i-1).getDateString(),tempCampArray.get(i-1).getRegClosingDateString(),slotString ,committeeString,tempCampArray.get(i-1).getStaffInCharge(), firstCommittee, ""};
+                this.csvFile.writeNext(firstCol);
+                // Print other camp committee members
+                for(int j=1; j< tempCampArray.get(i-1).getCampCommitteeList().size(); j++){
+                    String[] secondData = {"", "", "", "", "", "", "", tempCampArray.get(i-1).getCampCommitteeList().get(j).getUserID(), ""};
+                    this.csvFile.writeNext(secondData);
+                }
+                for(int k=0; k< tempCampArray.get(i-1).getCampStudentList().size(); k++){
+                    String[] teriaryData = {"", "", "", "", "", "", "", "", tempCampArray.get(i-1).getCampStudentList().get(k).getUserID()};
+                    this.csvFile.writeNext(teriaryData);
+                }
+            String[] blankCol = {"", "", "", "", "", "", "", "",""};
+            this.csvFile.writeNext(blankCol);
+            }
+            this.csvFile.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return true;
+    }
+    /*
+
+    public boolean writeInfo() throws IOException{
+        this.workbook = new XSSFWorkbook(); 
+        this.spreadsheet = this.workbook.createSheet(this.sheetName);
+        Map<String, Object[]> studentData = new TreeMap<String, Object[]>();
+        studentData.put("1", new Object[]{"CAMP Number", "Name", "Date", "Registration Closing Date", "Total slots", "Committee Members", "Staff-in-charge", "Committee Members", "Attendees"});
+        XSSFRow row;
+        Set<String> keyID = studentData.keySet();
+        int rowID = 0;
+        for(String key : keyID){
+            row = this.spreadsheet.createRow(rowID++);
+            Object[] objectArr = studentData.get(key);
+            int cellID = 0;
+            
+            for(Object obj:objectArr){
+                Cell cell = row.createCell(cellID++);
+                cell.setCellValue((String)obj);
+            }
+            
+        }
+        this.workbook.write(this.streamOut); 
+        this.streamOut.close(); 
+        return true;
+    }
+    */
+}
---
+++ anotherCAM3.0/anotherCAM/src/main/java/com/mycompany/anothercam/StaffApplicationManager/StaffPerformanceReport.java
@@ -0,0 +1,151 @@
+/**
+Represents the StaffPerformanceReportApp Object and the following assistance methods.
+A StaffPerformanceReportApp can be run multiple times depending on the number of reports to be made.
+@author Russel Tan Jun Hong
+@version 1.0
+@since 2023-11-3
+*/
+package com.mycompany.anothercam.StaffApplicationManager;
+import com.mycompany.anothercam.Camp;
+import com.mycompany.anothercam.DateFormatter;
+import com.mycompany.anothercam.Login.Verification;
+import com.mycompany.anothercam.User;
+import com.mycompany.anothercam.ReportGenerator.ReportTXTDAO;
+import com.mycompany.anothercam.ReportGenerator.ReportCSVDAO;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class StaffPerformanceReport {
+    
+    /**
+    * Runs the Staff Performance Report Application..
+    * @param campArray the CampArray which the new camp object is stored.
+    * @param cookie the User Object (A Staff) which the requested user creates the Camp
+    * @param scanObj takes in the scanner Object for automated testing.
+    * @return the boolean value whether the Staff Performance Report of camp object was successful.
+    */
+    public boolean runStaffPerformanceReport(ArrayList<Camp> campArray, User cookie, Scanner scanObj) throws IOException{
+        DateFormatter dateFor = new DateFormatter();
+        Verification verifier = new Verification();
+	//check if staff 
+		
+		//ask for filters
+		System.out.println("What filters would you like?");
+		System.out.println("0. Quit filter selection");
+                System.out.println("1. No Filter");
+		System.out.println("2. Filter by Student's faculty");
+                
+                // Find camp first
+                ArrayList<Camp> tempCampArray = new ArrayList<Camp>();
+                for(int i=0; i< campArray.size(); i++){
+                    if(campArray.get(i).getStaffInCharge().equals(cookie.getUserID())){
+                        tempCampArray.add(campArray.get(i));
+                    } 
+                }
+                
+                if(tempCampArray.size() <=0){
+                    System.out.println("There are no camps to create performance reports for!");
+                    return false;
+                }
+		//create string array
+                int filter = verifier.verifyScannerNumber(scanObj);
+                
+            boolean activateAlphabetical = false;
+            switch(filter){
+                case 0:
+                    return true;
+                case 1:
+                    break;
+                case 2:
+                    activateAlphabetical = true;
+                    System.out.println("Which faculty would");
+            }
+                //choose format
+		System.out.println("What format would you like the report to be?");
+                System.out.println("0. Exit Report Format");
+		System.out.println("1. txt");
+		System.out.println("2. csv");
+                
+                int format = verifier.verifyScannerNumber(scanObj);
+                FileWriter writer;
+                FileOutputStream  streamOut;
+		//check validity of input
+		//converting into txt
+                String filename = "staffPerformanceReport_"+dateFor.convert_datetime_to_string(LocalDateTime.now());
+                
+                if(format == 1){
+                    writer = new FileWriter(filename+".txt");
+                    writeInText(campArray, cookie, writer, dateFor, activateAlphabetical);
+                }else if(format == 2){
+                    streamOut = new FileOutputStream(new File(filename+".csv"));
+                    writeInCSV(campArray, cookie, streamOut, dateFor, filename+".csv", activateAlphabetical); 
+                }else if(format == 0){
+                    return false;
+                }else{
+                    System.out.println("Invalid Input!");
+                    return false;
+                }
+
+	return true;
+    }
+    
+    /**
+    * Runs the Staff Performance Report which is to be formatted to TXT.
+    * @param tempCampArray the temporary CampArray which the new camp object is stored.
+    * @param cookie the User Object (A Staff) which the requested user creates the Camp
+    * @param writer FileWriter object used to write information into the txt file.
+    * @param dateFor DateFormatter object used to format any dates required.
+    * @return the boolean value whether the TXT Report is saved or not.
+    */
+    public boolean writeInText(ArrayList<Camp> tempCampArray, User cookie, FileWriter writer, DateFormatter dateFor, boolean activateAlphabetical){
+            try{
+                ReportTXTDAO genReport = new ReportTXTDAO(writer, activateAlphabetical);
+                genReport.reportBeginnerHeader();
+                writer.write("PERFORMANCE REPORT FOR STAFF "+cookie.getUserID()+" \n");
+                genReport.reportGenerateTodayDateHeader(dateFor);
+                for(int i=1; i< tempCampArray.size()+1; i++){
+                    writer.write("============================================\n");
+                    writer.write("CAMP Number "+i+"\n");
+                    writer.write("CAMP Name: "+tempCampArray.get(i-1).getCampName()+" \n");
+                    
+                    if(tempCampArray.get(i-1).getCampCommitteeList().size() >0){
+                        writer.write("Camp Committee Member Name | Points Received \n");
+                        for(int j=0; j<tempCampArray.get(i-1).getCampCommitteeList().size(); j++){
+                            
+                            writer.write(tempCampArray.get(i-1).getCampCommitteeList().get(j).getUserID()+" "+tempCampArray.get(i-1).getCampCommitteeList().get(j).getPoints()+"\n");
+                        }
+                    }else{
+                         writer.write("No Students signed up for Camp as Attendees Yet! \n");
+                    }
+                }
+                genReport.reportEndingHeader();
+                writer.close();
+                }catch(Exception e){
+                    
+                 System.out.println(e);   
+                 return false;
+                }
+        return true;
+    }
+    
+    /**
+    * Runs the Staff Performance Report which is to be formatted to CSV.
+    * @param tempCampArray the temporary CampArray which the new camp object is stored.
+    * @param cookie the User Object (A Staff) which the requested user creates the Camp
+    * @param streamOut FileOutputStream object used to write information into the csv file.
+    * @param dateFor DateFormatter object used to format any dates required.
+    * @param filename String object where csv information will be saved into.
+    * @return the boolean value whether the CSV Report is saved or not.
+    */
+    public boolean writeInCSV(ArrayList<Camp> tempCampArray, User cookie, FileOutputStream streamOut, DateFormatter dateFor, String filename, boolean activateAlphabetical){
+        ReportCSVDAO genReport = new ReportCSVDAO(tempCampArray, filename, activateAlphabetical);
+        genReport.writeStaffPerformanceInfo();
+        return true;
+    }
+}
